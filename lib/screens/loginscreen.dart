beadc94b7ec228b47ef9d0b088fc0379bfed6936
Monarene <mekuleyimichael@gmail.com>
2021-02-15 17:22:41 +0100

    login screen is set 7

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -23,7 +23,92 @@ class _LoginScreenState extends State<LoginScreen> {
                 image: AssetImage('assets/images/login_background.jpg'),
                 fit: BoxFit.cover,
               ),
-            )
+            ),
+            Text(
+              "FRENZY",
+              style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 10.0),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 15),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: "Username",
+                    prefixIcon: Icon(
+                      Icons.account_box,
+                      size: 30,
+                    )),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 15),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: "Password",
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      size: 30,
+                    )),
+                obscureText: true,
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 60),
+                alignment: Alignment.center,
+                height: 45,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.5),
+                ),
+              ),
+            ),
+            Expanded(
+                child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Theme.of(context).primaryColor,
+                  height: 80,
+                  child: Text(
+                    "Don\'t have an account? Sign Up",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+            ))
           ],
         ),
       ),
